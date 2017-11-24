d8a3626ff83fd3316c6de8e232acc438a65c7c4b
Geert <geert.boer555@gmail.com>
2017-11-24 01:10:07 +0100

    SingleSensor toegevoegd

---
+++ SingleSensor/SingleSensor.ino
@@ -0,0 +1,21 @@
+#include "Sensor.hpp"
+
+Sensor sensor(A0, A1);/*
+Sensor sensor1(A2, A3);
+Sensor sensor2(A4, A5);
+Sensor sensor3(2, 3);*/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(sensor.readSensorCM());
+  /*
+  Serial.println(sensor1.readSensorCM());
+  Serial.println(sensor2.readSensorCM());
+  Serial.println(sensor3.readSensorCM());*/
+  delay(20);
+}
